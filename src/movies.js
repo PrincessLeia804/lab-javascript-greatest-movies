816a9dc6f52d30e4f097c9703c296010f4269613
Lea Model <lea.skywalker309@gmail.com>
2023-06-19 16:26:56 +0200

    3. Iteration finished

--- src/movies.js
+++ src/movies.js
@@ -16,19 +16,38 @@ function howManyMovies(moviesArray) {
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    
+    const allScores = moviesArray.map(movie => movie.score)
+    .filter(allScores => typeof allScores === 'number') 
+    .reduce((acc, movie) => acc + movie, 0);
+
+    if (moviesArray.length === 0) {
+        return 0;
+    }
+
+    let result = allScores / moviesArray.length;
+        return Math.round(result * 100) / 100;
+
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) { 
+    if(!moviesArray) return null;
+
+
+
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) { }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) { }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(moviesArray) {}
+function turnHoursToMinutes(moviesArray) { }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) { }
